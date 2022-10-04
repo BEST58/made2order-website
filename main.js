6df9da2760ed0bf9e6d83d2f2cf2765b403cc9d1
brett-k-cs <bkapla01@gmail.com>
2022-10-04 14:42:34 -0500

    Added some info to homepage & animations

---
+++ main.js
@@ -0,0 +1,19 @@
+// Scroll Observer (for animations)
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            if(!entry.target.classList.contains("show")) entry.target.classList.add("show")
+        } else {
+            if(entry.target.classList.contains("show")) entry.target.classList.remove("show")
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach(ele => observer.observe(ele));
+
+// IFrame/Video auto height:
+const videos = document.querySelectorAll(".video")
+videos.forEach(ele => {
+    ele.style.height = ele.clientWidth / 16 * 9 + "px"
+})
